f740d0d7a09b35e6e864e32099421127df5f7f90
HealerRr88 <rr88tino@gmail.com>
2025-07-11 14:03:37 +0700

    fix: disable lightningcss for Cloudflare build

--- next.config.ts
+++ next.config.ts
@@ -2,24 +2,26 @@ import type { NextConfig } from "next"
 import createNextIntlPlugin from "next-intl/plugin"
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true
-  },
-  images: {
-    unoptimized: true
-  },
-  output: "export", // ⚠️ Quan trọng để export static!
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"]
-    });
-    return config;
-  }
-};
-
+	reactStrictMode: true,
+	experimental: {
+		optimizeCss: false // 👈 Tắt lightningcss
+	},
+	eslint: {
+		ignoreDuringBuilds: true
+	},
+	images: {
+		unoptimized: true
+	},
+	output: "export", // ⚠️ Quan trọng để export static!
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			use: ["@svgr/webpack"]
+		})
+		return config
+	}
+}
 
 const withNextIntl = createNextIntlPlugin()
 
